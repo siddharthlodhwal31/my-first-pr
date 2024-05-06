2e1743574c2f69ddf6fcbe8cf7c3df42015ad828
siddharth <siddharth311201@gmail.com>
2024-05-06 14:22:01 +0530

    c pro

---
+++ Contributors/volcanus/c/index.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(){
+    float a,b;
+
+    printf("enter two numbers  : ");
+    scanf("%f%f",&a,&b);
+     printf("the total number of devisible is %f",a/b);
+     printf("\n the total number of sum is %f",a+b);
+     printf("\n the total number of sub is %f",a-b);
+     printf("\n the total number of multiplication is %f",a*b);      
+}
